8e23225baa0c3052e043519723e1096841171c64
Jhon Vargas <jhon89nbl@gmail.com>
2022-03-18 14:54:36 -0500

    ajuste de paginas

--- src/main/java/com/jhon89nbl/programpos/controller/PrincipalController.java
+++ src/main/java/com/jhon89nbl/programpos/controller/PrincipalController.java
@@ -1,10 +1,13 @@
 package com.jhon89nbl.programpos.controller;
 
+import com.jhon89nbl.programpos.model.User;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
@@ -15,24 +18,15 @@ import java.util.ResourceBundle;
 public class PrincipalController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        panePrincipal.sceneProperty().addListener(((observableValue, oldScene, newScene) ->{
-            if (oldScene == null && newScene !=null){
-                newScene.windowProperty().addListener(((observableValue1, oldWindow, newWindow) ->{
-                    if(oldWindow==null && newWindow != null){
-                        ((Stage) newWindow).maximizedProperty().addListener((a,b,c)->{
-                            if(c){
-                                System.out.println(c);
-                            }
-                        });
-                    }
-                } ));
-            }
-
-        } ));
+        lblTitle.setText(user.getName()+ " " + user.getLastName());
     }
 
+    User user = User.getINSTANCE();
+
+
     @FXML
-    private AnchorPane panePrincipal;
+    private Label lblTitle;
+
 
 
 
