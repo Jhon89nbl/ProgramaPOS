328e82af95c31af8321326a5e22057a6727e40fb
Jhon89nbl <S@nti2015>
2022-05-16 07:42:03 -0500

    ordenes

--- src/main/java/com/jhon89nbl/programpos/controller/PrincipalController.java
+++ src/main/java/com/jhon89nbl/programpos/controller/PrincipalController.java
@@ -37,7 +37,7 @@ public class PrincipalController implements Initializable {
     @FXML
     private AnchorPane paneCenter;
 
-
+    //se crea lista para cargar las ventanas
     private final List<Region> window= new ArrayList<>();
 
 
@@ -46,16 +46,24 @@ public class PrincipalController implements Initializable {
 
     @FXML
     void goAddProduct(ActionEvent event) throws MalformedURLException {
+        //se carga la uri en un string
         String PRODUCT = "src/main/resources/com/jhon89nbl/programpos/add-product.fxml";
+        //validamos si la lista de ventana esta vacia
         if(window.isEmpty()){
+            //si esta vacia se carga la ventana de producto
             chargeWindow(Paths.get(PRODUCT).toUri().toURL(),
                     "product");
         }else {
+            //se valida por el id si ya esta creada en la ventana
             if(searchWindow("product")){
+                //si esta abierta muestra error
                 errorWindow();
             }else {
+                //en caso contrario se remueve todas las ventanas
                 paneCenter.getChildren().removeAll();
+                //se limpia el pane center
                 paneCenter.getChildren().clear();
+                //se carga nueva mente la ventana
                 chargeWindow(Paths.get(PRODUCT).toUri().toURL(),
                         "product");
             }
@@ -64,16 +72,24 @@ public class PrincipalController implements Initializable {
 
     @FXML
     void goAddCategory(ActionEvent event) throws MalformedURLException {
+        //se carga la uri en un string
         String CATEGORY = "src/main/resources/com/jhon89nbl/programpos/category.fxml";
+        //validamos si la lista de ventana esta vacia
         if(window.isEmpty()){
+            //si esta vacia se carga la ventana de producto
             chargeWindow(Paths.get(CATEGORY).toUri().toURL(),
                     "category");
         }else {
+            //se valida por el id si ya esta creada en la ventana
             if(searchWindow("category")){
+                //si esta abierta muestra error
                 errorWindow();
             }else {
+                //en caso contrario se remueve todas las ventanas
                 paneCenter.getChildren().removeAll();
+                //se limpia el pane center
                 paneCenter.getChildren().clear();
+                //se carga nueva mente la ventana
                 chargeWindow(Paths.get(CATEGORY).toUri().toURL(),
                         "category");
             }
@@ -82,16 +98,24 @@ public class PrincipalController implements Initializable {
     }
     @FXML
     void goAddSales(ActionEvent event) throws MalformedURLException {
+        //se carga la uri en un string
         String SALES = "src/main/resources/com/jhon89nbl/programpos/sales.fxml";
+        //validamos si la lista de ventana esta vacia
         if(window.isEmpty()){
+            //si esta vacia se carga la ventana de producto
             chargeWindow(Paths.get(SALES).toUri().toURL(),
                     "sales");
         }else {
+            //se valida por el id si ya esta creada en la ventana
             if(searchWindow("sales")){
+                //si esta abierta muestra error
                 errorWindow();
             }else {
+                //en caso contrario se remueve todas las ventanas
                 paneCenter.getChildren().removeAll();
+                //se limpia el pane center
                 paneCenter.getChildren().clear();
+                //se carga nueva mente la ventana
                 chargeWindow(Paths.get(SALES).toUri().toURL(),
                         "sales");
             }
@@ -100,33 +124,52 @@ public class PrincipalController implements Initializable {
 
     @FXML
     void goAddProvider(ActionEvent event) throws MalformedURLException {
+        //se carga la uri en un string
         String PROVIDER = "src/main/resources/com/jhon89nbl/programpos/add-provider.fxml";
+        //validamos si la lista de ventana esta vacia
         if(window.isEmpty()){
+            //si esta vacia se carga la ventana de producto
             chargeWindow(Paths.get(PROVIDER).toUri().toURL(),
                     "provider");
         }else {
+            //se valida por el id si ya esta creada en la ventana
             if(searchWindow("provider")){
+                //si esta abierta muestra error
                 errorWindow();
             }else {
+                //en caso contrario se remueve todas las ventanas
                 paneCenter.getChildren().removeAll();
+                //se limpia el pane center
                 paneCenter.getChildren().clear();
+                //se carga nueva mente la ventana
                 chargeWindow(Paths.get(PROVIDER).toUri().toURL(),
                         "provider");
             }
         }
     }
 
+    @FXML
+    void goOrders(ActionEvent event) {
+
+    }
+
     private void chargeWindow(URL fxmURL, String id){
         try {
+            // se obtiene la escena del pane
             Scene scene = paneCenter.getScene();
+            //se elecciona el tamaño preferido del pane
             paneCenter.setPrefSize(scene.getWidth(),scene.getHeight()*0.83);
+            //se carga la url en la region
             Region region = FXMLLoader.load(fxmURL);
+            //se le asigna un id para identificarla
             region.setId(id);
+            //se añade la region al pane
             paneCenter.getChildren().add(region);
+            // se ajusta el tamaño de la region al tamañp del pane
             region.prefWidthProperty().bind(paneCenter.widthProperty());
             region.prefHeightProperty().bind(paneCenter.heightProperty());
+            // se añade la region a la a la lista de ventana
             window.add(region);
-
 
 
         } catch (IOException e) {
@@ -142,10 +185,13 @@ public class PrincipalController implements Initializable {
     }
 
     private boolean searchWindow(String id){
+        //se crea variable para validar
         boolean ischarge = false;
+        // se busca en la lista de ventanas si la region ya esta creada por medio del ID
         for (Region region: window) {
             ischarge = region.getId().equals(id);
         }
+        //se retorna la validacion
         return ischarge;
     }
 
